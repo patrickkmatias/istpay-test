42aea94566ef73c96cd83a308d75eb1baa1607f4
Patrick <99044724+pMattheew@users.noreply.github.com>
2023-04-25 11:37:40 -0300

    feat: gitpod support (#1)

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,48 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+
+const vitePort = 5173;
+
+/**
+ * Checks if any GitPod/Codespaces environment variable
+ * is present and returns its Vite port URL; otherwise
+ * returns null.
+ *
+ * @returns GitPod/Codespaces URL | null
+ */
+const getCloudEnv = () => {
+    const {
+        GITPOD_WORKSPACE_URL,
+        CODESPACE_NAME,
+        GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN,
+    } = process.env;
+    if (GITPOD_WORKSPACE_URL) {
+        return `${vitePort}-${GITPOD_WORKSPACE_URL.substring(
+            "https://".length
+        )}`;
+    } else if (CODESPACE_NAME) {
+        return `${CODESPACE_NAME}-${vitePort}.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`;
+    }
+    return null;
+};
+
+const server = getCloudEnv()
+    ? {
+          port: vitePort,
+          host: "0.0.0.0",
+          hmr: {
+              host: getCloudEnv(),
+              clientPort: 443,
+              protocol: 'wss'
+          },
+      }
+    : undefined;
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: "resources/js/app.js",
             refresh: true,
         }),
         vue({
@@ -17,4 +54,5 @@ export default defineConfig({
             },
         }),
     ],
+    server,
 });

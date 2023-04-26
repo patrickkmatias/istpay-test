907de8b651cd49a1d1553079b221ba8cc88fae97
Patrick M <patrickmatias.silva@hotmail.com>
2023-04-26 14:25:04 +0000

    refactor(db): use constants class, column type enum

--- database/migrations/2023_04_25_181620_create_jobs_table.php
+++ database/migrations/2023_04_25_181620_create_jobs_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Job;
+use Constants\Constants;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->timestamps();
             $table->string('title');
             $table->text('description')->nullable();
-            $table->set('type', Job::JOB_TYPES);
+            $table->enum('type', Constants::JOB_TYPES);
             $table->boolean('paused')->default(false);
         });
     }

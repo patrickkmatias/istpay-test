2731a9b583d8e5eb380681c2a16184c39e5162d9
Patrick M <patrickmatias.silva@hotmail.com>
2023-04-26 18:02:07 +0000

    test(Job): initial jobs controller test, rm mock tests

---
+++ tests/Feature/Jobs/IndexJobsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Inertia\Testing\AssertableInertia as Assert;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class IndexJobsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_jobs_data_is_displayed(): void
+    {
+        $user = User::factory()->create();
+
+        $this->seed();
+
+        $response = $this
+            ->actingAs($user)
+            ->get('/jobs')->assertInertia(
+                fn(Assert $page) => $page
+                    ->component('Jobs/List')
+                    ->has(
+                        'jobs', fn(Assert $jobs) => $jobs
+                            ->has('data', 10)
+                            ->has('links')
+                            ->has('meta')
+                    )
+                    ->has(
+                        'filters', fn(Assert $filters) => $filters
+                            ->where('type', null)
+                            ->where('query', null)
+                    )
+            );
+
+        $response->assertOk();
+    }
+}

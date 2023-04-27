1dc07e40cd80e28f6259f89b1cabe1cf5f68bfe0
Patrick M <patrickmatias.silva@hotmail.com>
2023-04-27 18:39:01 +0000

    test(Job): add CreateJobTest

---
+++ tests/Feature/Jobs/CreateJobTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Inertia\Testing\AssertableInertia as Assert;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CreateJobTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_job_can_be_created()
+    {
+        $user = User::factory()->create();
+
+        $path = '/jobs';
+
+        $data = [
+            'title' => 'Test job',
+            'description' => 'Test job description',
+            'type' => 'CLT',
+            'paused' => false,
+        ];
+
+        $this->actingAs($user)->post($path, $data)->assertSee($data)->assertCreated();
+
+        $data = array_diff_key($data, ['paused' => false]);
+
+        $this->actingAs($user)->post($path, $data)->assertCreated();
+
+        $data = array_diff_key($data, ['description' => '']);
+
+        $this->actingAs($user)->post($path, $data)->assertCreated();
+    }
+
+    public function test_throw_if_no_required_fields()
+    {
+        $user = User::factory()->create();
+
+        $path = '/jobs';
+
+        $data = ['title' => 'Test job'];
+
+        $this->actingAs($user)->post($path, $data)->assertFound();
+
+        $data = ['type' => 'CLT'];
+
+        $this->actingAs($user)->post($path, $data)->assertFound();
+    }
+
+    public function test_throw_if_invalid_input()
+    {
+        $user = User::factory()->create();
+
+        $path = '/jobs';
+
+        $data = [
+            'title' => 'Test job',
+            'type' => 'Internship',
+        ];
+
+        $this->actingAs($user)->post($path, $data)->assertFound();
+
+        $data = [
+            'title' => 'averybignameaverybignameaverybignameaverybignameaverybignameaverybignameaverybignameaverybignameaverybignameaverybignameaverybignameaverybignameaverybignameaverybignameaverybignameaverybignameaverybignameaverybignameaverybigname',
+            'type' => 'CLT',
+        ];
+
+        $this->actingAs($user)->post($path, $data)->assertFound();
+    }
+}

4a705962eeb3b0f410169e39aa26ab0bfee4de50
Patrick M <patrickmatias.silva@hotmail.com>
2023-05-01 01:37:48 +0000

    feat(Job): factory creating user w/ candidate

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\Models\Candidate;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -21,7 +23,8 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            // password
             'remember_token' => Str::random(10),
         ];
     }
@@ -31,8 +34,15 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (User $user) {
+            Candidate::factory()->create(['user_id' => $user->id]);
+        });
     }
 }
